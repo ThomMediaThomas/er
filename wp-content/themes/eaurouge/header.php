fb0f746c57d0c38a59edbb9ee6a5263d67ea3a2a
Thomas <thomas.bartels@nos.nl>
2018-10-24 12:11:34 +0100

    Fix in pagetitle-separator

--- wp-content/themes/eaurouge/header.php
+++ wp-content/themes/eaurouge/header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?> class="no-js">
 	<head>
 		<meta charset="<?php bloginfo('charset'); ?>">
-		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
+		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' |'; } ?> <?php bloginfo('name'); ?></title>
 
 		<link href="//fonts.googleapis.com/css?family=Dosis|Roboto|Caveat+Brush" rel="stylesheet">
 		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/styles/main.css">
