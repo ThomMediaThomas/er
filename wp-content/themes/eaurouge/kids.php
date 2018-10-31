bec8d2632044578002b8439d772c3087d419f3b0
Thomas <thomas.bartels@nos.nl>
2018-10-31 19:56:58 +0100

    W0006: started with kids-page

---
+++ wp-content/themes/eaurouge/kids.php
@@ -0,0 +1,12 @@
+<?php /* Template Name: Kids corner */ ?>
+
+<?php get_header(); ?>
+
+	<main role="main" class="kids-page">
+
+		<?php get_template_part('/elements/hero'); ?>
+		<?php get_template_part('/elements/custom-blocks'); ?>
+
+	</main>
+
+<?php get_footer(); ?>
