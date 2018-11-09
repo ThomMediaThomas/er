7eb95f2dc6b49249994c03abd7631307fb763da3
Thomas <thomas@thommedia.nl>
2018-11-09 12:25:12 +0100

    W0100: logo in menu

--- wp-content/themes/eaurouge/elements/site-header.php
+++ wp-content/themes/eaurouge/elements/site-header.php
@@ -12,6 +12,9 @@
     <a id="show-nav" href="#" class="show-nav button yellow show-on-mobile"><i class="icon-menu"></i> Menu</a>
     <nav id="nav">
         <div class="content-wrapper">
+            <a href="/" title="Camping de l'eau rouge" class="logo-holder" id="show-on-mobile">
+                <img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/logo.png" title="Domaine de l'eau rouge" alt="Domaine de l'eau rouge" />
+            </a>
             <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
         </div>
         <a id="hide-nav" href="#" class="show-nav button yellow show-on-mobile"><i class="icon-close"></i></a>
