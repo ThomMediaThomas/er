9634c709235a8359e98c799c9cc8e74750668ef9
Thomas <thomas@thommedia.nl>
2018-11-20 12:27:10 +0100

    Missing change

--- wp-content/themes/eaurouge/footer.php
+++ wp-content/themes/eaurouge/footer.php
@@ -3,7 +3,6 @@
 		<script src="<?php echo get_template_directory_uri(); ?>/scripts/vendor/jquery.2.2.4.min.js"></script>
 		<script src="<?php echo get_template_directory_uri(); ?>/scripts/vendor/datepicker.1.0.0.js"></script>
 		<script src="<?php echo get_template_directory_uri(); ?>/scripts/vendor/datepicker.nl-NL.js"></script>
-		<script src="<?php echo get_template_directory_uri(); ?>/scripts/vendor/jquery.sticky-kit.min.js"></script>
 		<script src="<?php echo get_template_directory_uri(); ?>/scripts/helpers.js"></script>
 		<script src="<?php echo get_template_directory_uri(); ?>/scripts/main.js"></script>
 	</body>
