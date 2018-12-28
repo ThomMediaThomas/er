9a8416f17236dc80837f58ffe98b4db208965269
Thomas <thomas@thommedia.nl>
2018-12-28 19:57:58 +0100

    W0161: normal text-page

--- wp-content/themes/eaurouge/page.php
+++ wp-content/themes/eaurouge/page.php
@@ -5,6 +5,11 @@
 	<main role="main">
 
 		<?php get_template_part('/elements/hero'); ?>
+		<?php if (!empty(get_the_content())): ?>
+			<div class="content-wrapper">
+				<?php the_content(); ?>
+			</div>
+		<?php endif; ?>
 		<?php get_template_part('/elements/custom-blocks'); ?>
 
 	</main>
