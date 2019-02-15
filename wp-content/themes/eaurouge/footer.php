480deb90f983742970e57843872898c89c448a51
Thomas <thomas@thommedia.nl>
2019-02-15 22:25:24 +0100

    W0237: bump version number

--- wp-content/themes/eaurouge/footer.php
+++ wp-content/themes/eaurouge/footer.php
@@ -1,10 +1,11 @@
 		<?php get_template_part('/elements/site-footer'); ?>
 		<?php wp_footer(); ?>
+		<?php $scriptVersion = 2; ?>
 		<script src="<?php echo get_template_directory_uri(); ?>/scripts/vendor/jquery.2.2.4.min.js"></script>
 		<script src="<?php echo get_template_directory_uri(); ?>/scripts/vendor/datepicker.1.0.0.js"></script>
 		<script src="<?php echo get_template_directory_uri(); ?>/scripts/vendor/datepicker.nl-NL.js"></script>
 		<script src="<?php echo get_template_directory_uri(); ?>/scripts/vendor/date.format.js"></script>
-		<script src="<?php echo get_template_directory_uri(); ?>/scripts/helpers.js"></script>
-		<script src="<?php echo get_template_directory_uri(); ?>/scripts/main.js"></script>
+		<script src="<?php echo get_template_directory_uri(); ?>/scripts/helpers.js?v=<?php echo $scriptVersion; ?>"></script>
+		<script src="<?php echo get_template_directory_uri(); ?>/scripts/main.js?v=<?php echo $scriptVersion; ?>"></script>
 	</body>
 </html>
