bea2584606f3b91c03af4b33edb2e99207a1f589
Thomas <thomas@thommedia.nl>
2020-12-27 20:31:46 +0100

    W0298: Small fix for ugly alignment after error

--- wp-content/themes/eaurouge/elements/accommodation-booker/selected-family.php
+++ wp-content/themes/eaurouge/elements/accommodation-booker/selected-family.php
@@ -1,63 +1,67 @@
 <?php the_field('family_intro', 'options'); ?>
 <div class="family">
-    <div class="input-field">
-        <?php $adults = isset($_GET['adults']) ? $_GET['adults'] : 1; ?>
-        <label for="adults"><?php _e('Volwassenen', 'eaurouge'); ?>:</label>
-        <div class="counter">
-            <span class="number-input">
-                <span class="input-with-icon">
-                    <i class="adults"></i>
-                    <input name="adults" id="adults" class="required change-family-members" type="number" placeholder="0" value="<?php echo $adults; ?>">
+    <div class="clearfix">
+        <div class="input-field">
+            <?php $adults = isset($_GET['adults']) ? $_GET['adults'] : 1; ?>
+            <label for="adults"><?php _e('Volwassenen', 'eaurouge'); ?>:</label>
+            <div class="counter">
+                <span class="number-input">
+                    <span class="input-with-icon">
+                        <i class="adults"></i>
+                        <input name="adults" id="adults" class="required change-family-members" type="number" placeholder="0" value="<?php echo $adults; ?>">
+                    </span>
+                    <i class="up">+</i>
+                    <i class="down">-</i>
                 </span>
-                <i class="up">+</i>
-                <i class="down">-</i>
+            </div>
+        </div>
+        <div class="input-field">
+            <?php $children = isset($_GET['children']) ? $_GET['children'] : null; ?>
+            <label for="children"><?php _e('Kinderen (4 t/m 15 jaar)', 'eaurouge'); ?>:</label>
+            <div class="counter">
+                <span class="number-input">
+                    <span class="input-with-icon">
+                        <i class="children"></i>
+                        <input name="children" id="children" class="change-family-members" type="number" placeholder="0" value="<?php echo $children; ?>">
+                    </span>
+                    <i class="up">+</i>
+                    <i class="down">-</i>
+                </span>
+            </div>
+        </div>
+    </div>    
+    <div class="clearfix">
+        <div class="input-field">
+            <?php $babies = isset($_GET['babies']) ? $_GET['babies'] : null; ?>
+            <label for="babies"><?php _e('Kinderen (0 t/m 3 jaar)', 'eaurouge'); ?>:</label>
+            <div class="counter">
+                <span class="number-input">
+                    <span class="input-with-icon">
+                        <i class="babies"></i>
+                        <input name="babies" id="babies" class="change-family-members" type="number" placeholder="0" value="<?php echo $babies; ?>">
+                    </span>
+                    <i class="up">+</i>
+                    <i class="down">-</i>
+                </span>
+            </div>
+        </div>
+        <div class="input-field">
+            <?php $pets = isset($_GET['pets']) ? $_GET['pets'] : null; ?>
+            <label for="pets"><?php _e('Huisdieren', 'eaurouge'); ?>:</label>
+            <div class="counter">
+                <span class="number-input">
+                    <span class="input-with-icon">
+                        <i class="pets"></i>
+                        <input name="pets" id="pets" type="number" placeholder="0" value="<?php echo $pets; ?>">
+                    </span>
+                    <i class="up">+</i>
+                    <i class="down">-</i>
+                </span>
+            </div>
+            <span class="smaller input-sub regular">
+                <?php _e('Kamperen: maximaal 2', 'eaurouge'); ?><br />
+                <?php _e('Chalet: maximaal 1', 'eaurouge'); ?>
             </span>
         </div>
-    </div>
-    <div class="input-field">
-        <?php $children = isset($_GET['children']) ? $_GET['children'] : null; ?>
-        <label for="children"><?php _e('Kinderen (4 t/m 15 jaar)', 'eaurouge'); ?>:</label>
-        <div class="counter">
-            <span class="number-input">
-                <span class="input-with-icon">
-                    <i class="children"></i>
-                    <input name="children" id="children" class="change-family-members" type="number" placeholder="0" value="<?php echo $children; ?>">
-                </span>
-                <i class="up">+</i>
-                <i class="down">-</i>
-            </span>
-        </div>
-    </div>
-    <div class="input-field">
-        <?php $babies = isset($_GET['babies']) ? $_GET['babies'] : null; ?>
-        <label for="babies"><?php _e('Kinderen (0 t/m 3 jaar)', 'eaurouge'); ?>:</label>
-        <div class="counter">
-            <span class="number-input">
-                <span class="input-with-icon">
-                    <i class="babies"></i>
-                    <input name="babies" id="babies" class="change-family-members" type="number" placeholder="0" value="<?php echo $babies; ?>">
-                </span>
-                <i class="up">+</i>
-                <i class="down">-</i>
-            </span>
-        </div>
-    </div>
-    <div class="input-field">
-        <?php $pets = isset($_GET['pets']) ? $_GET['pets'] : null; ?>
-        <label for="pets"><?php _e('Huisdieren', 'eaurouge'); ?>:</label>
-        <div class="counter">
-            <span class="number-input">
-                <span class="input-with-icon">
-                    <i class="pets"></i>
-                    <input name="pets" id="pets" type="number" placeholder="0" value="<?php echo $pets; ?>">
-                </span>
-                <i class="up">+</i>
-                <i class="down">-</i>
-            </span>
-        </div>
-        <span class="smaller input-sub regular">
-            <?php _e('Kamperen: maximaal 2', 'eaurouge'); ?><br />
-            <?php _e('Chalet: maximaal 1', 'eaurouge'); ?>
-        </span>
     </div>
 </div>
